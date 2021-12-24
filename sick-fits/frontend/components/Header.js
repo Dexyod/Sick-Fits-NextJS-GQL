d4727cef76850010d416bfcde1b898a0dc2455d0
Declan O'Donnell <dexyod43@gmail.com>
2021-12-23 20:54:11 -0500

    fixed linting rules

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,13 +1,21 @@
 import Link from 'next/link';
 import Nav from './Nav';
+import styled from 'styled-components';
+
+const Logo = styled.h1`
+  background: red;
+  a {
+    color: white;
+  }
+`;
 
 export default function Header() {
   return (
     <header>
       <div className='bar'>
-        <Link href='/'>
-          <h1>Sick Fits</h1>
-        </Link>
+        <Logo>
+          <Link href='/'>Sick Fits</Link>
+        </Logo>
       </div>
       <div className='sub-bar'>
         <p>Search</p>
--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -4,7 +4,7 @@ export default class MyDocumnet extends Document {
   render() {
     return (
       <Html lang='en-CA'>
-        <Head></Head>
+        {/* <Head></Head> */}
         <body>
           <Main />
           <NextScript />
